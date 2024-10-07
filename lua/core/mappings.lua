5d4e2197152ff33aa2585eda8c71b56e54970488
Gokul Prathin A <gokulprathin8@gmail.com>
2024-10-07 16:09:03 -0400

    nvim tree show .env

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -10,8 +10,8 @@ M.general = {
 
     -- navigate within insert mode
     ["<C-h>"] = { "<Left>", "Move left" },
-    ["<C-l>"] = { "<Right>", "Move right" },
-    ["<C-j>"] = { "<Down>", "Move down" },
+    -- ["<C-l>"] = { "<Right>", "Move right" },
+    -- ["<C-j>"] = { "<Down>", "Move down" },
     ["<C-k>"] = { "<Up>", "Move up" },
   },
 
